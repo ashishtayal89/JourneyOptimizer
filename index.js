37cdc82c9319f256b25e14bffbd1dde93b6bc856
Ashish Tayal <ashishtayal@Ashishs-MacBook-Air.local>
2022-10-07 18:19:45 +0530

    Fix entry path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var liveServer = require("live-server");
 
 var params = {
   port: 8080, // Set the server port. Defaults to 8080.
-  root: "./demo", // Set root directory that's being served. Defaults to cwd.
+  root: "./", // Set root directory that's being served. Defaults to cwd.
 };
 
 liveServer.start(params);
