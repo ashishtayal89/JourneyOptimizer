6b60987fc75f832b1b2166003e53dc09cdd598b3
Ashish Tayal <ashishtayal@Ashishs-MacBook-Air.local>
2022-10-07 18:28:19 +0530

    Remove port

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var liveServer = require("live-server");
 
 var params = {
-  port: 8080, // Set the server port. Defaults to 8080.
   root: "./", // Set root directory that's being served. Defaults to cwd.
 };
 
