91aba1e1eeb666457957cca136af351690f1789b
Ashish Tayal <ashishtayal@Ashishs-MacBook-Air.local>
2022-10-07 18:14:04 +0530

    Boilerplate

---
+++ Reamme.md
@@ -0,0 +1,3 @@
+## Journey Optimizer
+
+This is a POC of the flow diagram creator based on vanila javascript.
---
+++ index.js
@@ -0,0 +1,8 @@
+var liveServer = require("live-server");
+
+var params = {
+  port: 8080, // Set the server port. Defaults to 8080.
+  root: "./demo", // Set root directory that's being served. Defaults to cwd.
+};
+
+liveServer.start(params);
